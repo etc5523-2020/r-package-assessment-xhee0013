55610040d3252bc760fdc3f13e737a808c76b4b0
XITONG HE <xhee0013@student.monash.edu>
2020-10-20 11:03:30 +1100

    adding the data

---
+++ R/covid19_count.R
@@ -0,0 +1,19 @@
+#'covid19_count
+#'
+#'This data set displays the monthly coronavirus cases in each country with each region in each country
+#'
+#'@format In total there are 1395 observations across 7 variables.
+#' 
+#' - **Type**: coronavirus case type being confirmed,death or recovered.
+#' - **province**: Name of province/state, for countries. 
+#' - **Month**: Each month variable.
+#' - **Country**: Name of country/region. 
+#' - **long**: Longitude of center of geographic region.
+#' - **lat**:Latitude of center of geographic region.
+#' - **Cases**:Number of cases on given month.
+#'
+#' @import tibble
+#' 
+#' @source [Coronavirus Github](https://github.com/RamiKrispin/coronavirus)
+
+"covid19_count"
